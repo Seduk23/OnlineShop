16709aab7b07a16bbbc65f52f90221106b3b284f
Seduk23 <vanyasedih23@ya.ru>
2024-01-23 23:00:35 +0300

    Final

--- LaTex/kod/about_view.py
+++
@@ -1,7 +0,0 @@
-from templates_view.base_view import View
-from render_template import render_template
-
-class AboutView(View):
-    template = 'templates/about.html'
-    def get(self, environ):
-        return render_template(template_name=self.template, context={})
